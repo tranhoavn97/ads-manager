7162c1108a069a6bc2df1d0c629b0f44352b40f1
tranhoavn97 <48686049+tranhoavn97@users.noreply.github.com>
2026-06-26 23:31:15 +0700

    feat: use existing post Shopee server entrypoint

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 // Điểm vào serverless cho Vercel: nạp app Express và xuất ra làm handler.
 // Mọi request /api/* được vercel.json định tuyến tới đây.
-import app from '../server.js';
+import app from '../server-existing.js';
 
 export default app;
